771db2c305894866523ca3ecc9d20a3291096be7
Paulo Gualter <paulogualter@gmail.com>
2025-10-07 15:16:58 -0300

    FIX BUILD: Remove deprecated appDir config, add Node.js 20 requirement

--- next.config.js
+++ next.config.js
@@ -1,8 +1,5 @@
 /** @type {import('next').NextConfig} */
 const nextConfig = {
-  experimental: {
-    appDir: true,
-  },
   images: {
     domains: ['localhost', 'supabase.co', 'images.unsplash.com'],
     remotePatterns: [
