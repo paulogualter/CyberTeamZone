2629e8c25298af9fcc58d95b29924cd8be67a4c6
Paulo Gualter <paulogualter@gmail.com>
2025-10-07 15:13:24 -0300

    FINAL FIX: Move all files to root, remove vercel configs, enable auto-detection

--- vercel/path0/components/OAuthHandler.tsx
+++
@@ -1,8 +0,0 @@
-'use client'
-
-import { useOAuthAccount } from '@/hooks/useOAuthAccount'
-
-export default function OAuthHandler() {
-  useOAuthAccount()
-  return null // Este componente não renderiza nada, apenas executa o hook
-}
