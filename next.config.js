afac1cfe9aa2153b14eff7a7c04425a591195d7e
Paulo Gualter <paulogualter@gmail.com>
2025-10-07 15:20:57 -0300

    FIX ALIAS PATHS: Add specific webpack aliases and tsconfig paths for hooks

--- next.config.js
+++ next.config.js
@@ -13,6 +13,10 @@ const nextConfig = {
     config.resolve.alias = {
       ...config.resolve.alias,
       '@': require('path').resolve(__dirname, '.'),
+      '@/components': require('path').resolve(__dirname, './components'),
+      '@/lib': require('path').resolve(__dirname, './lib'),
+      '@/hooks': require('path').resolve(__dirname, './hooks'),
+      '@/types': require('path').resolve(__dirname, './types'),
     }
     return config
   },
